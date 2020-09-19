7805117d520bec0d78f7d8e2eb5bf1c0318cc753
Gonçalves de Souza <togz@gft.com>
2020-09-19 00:35:15 -0300

    Reutilizando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== Test 1 : seller findById =====");
 		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 
