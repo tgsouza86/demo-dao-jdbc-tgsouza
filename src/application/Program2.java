043641f80ed2310d4f723e877649804e8a5762ad
Gonçalves de Souza <togz@gft.com>
2020-09-20 00:05:30 -0300

    Find all Departments

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -15,6 +17,12 @@ public class Program2 {
 		
 		Department department =  departmentDao.findById(1);
 		System.out.println(department);
+		
+		System.out.println("\n=== Test 2: seller findByDepartment=====");
+		List<Department> list = departmentDao.findAll();		
+		for (Department d : list) {
+			System.out.println(d);
+		}
 
 	}
 
