18c31ef116ba507456decfd55842b08447d7007f
Gonçalves de Souza <togz@gft.com>
2020-09-21 16:42:39 -0300

    Inserir Dados Departamento

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+
 import model.entities.Department;
 
 public class Program2 {
@@ -23,6 +24,11 @@ public class Program2 {
 		for (Department d : list) {
 			System.out.println(d);
 		}
+		
+		System.out.println("\n=== Test 4: seller insert=====");
+		Department newDepartment = new Department(null, "Musica");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 
 	}
 
